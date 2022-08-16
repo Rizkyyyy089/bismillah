e7b15e68599308dd0bc1ec7f7606c51cd0c71185
Rizkyyyy089 <111153045+Rizkyyyy089@users.noreply.github.com>
2022-08-16 22:21:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.APIKeys = {
 }
 
 // NAMBAH BOLEH HAPUS JANGAN -_-
-global.owner = ['6281511480762']
-global.premium = ['621511480762']
-global.packname = 'AzzBot√'
-global.author = 'AzzBot-MD'
+global.owner = ['62881025138150']
+global.premium = ['62881025138150']
+global.packname = 'UusslowBOTz'
+global.author = 'UusslowBOTz'
 global.sessionName = 'Xavior'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
